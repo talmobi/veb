553d350c0d12ec77fb15e3c459e0a543c074d2a4
Hannyajin <talmo.christian@gmail.com>
2014-05-11 14:53:57 +0300

    cleaning up

--- Gruntfile.js
+++ Gruntfile.js
@@ -166,9 +166,9 @@ module.exports = function (grunt) {
                     src: [
                         '<%= config.dist %>/scripts/{,*/}*.js',
                         '<%= config.dist %>/styles/{,*/}*.css',
-                        '<%= config.dist %>/images/{,*/}*.*',
-                        '<%= config.dist %>/styles/fonts/{,*/}*.*',
-                        '<%= config.dist %>/*.{ico,png}'
+                        //'<%= config.dist %>/images/{,*/}*.*', // don't rename images
+                        '<%= config.dist %>/styles/fonts/{,*/}*.*'
+                        // '<%= config.dist %>/*.{ico,png}' // don't rename icons
                     ]
                 }
             }
