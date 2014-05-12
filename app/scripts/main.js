5519a0cee45cf3ec6892b7a900c4da53006b73eb
Hannyajin <talmo.christian@gmail.com>
2014-05-12 20:51:18 +0300

    older phone WURLF fixed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,7 @@
 eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('3 2={"1":0,"4":"5 8 7","6":"9"};',10,10,'false|is_mobile|WURFL|var|complete_device_name|generic|form_factor|browser|web|Desktop'.split('|'),0,{}));
 
+console.log(WURFL);
+
 var VERILY = (function() {
 	// private fields
 	var root, widget, mobile
@@ -14,9 +16,14 @@ var VERILY = (function() {
 		root = $('#appRoot');
 		mobile = false;
 		//if (window.innerWidth <= 480) { // assume it's a mobile
-		if (WURFL.is_mobile) { // assume it's a mobile
+		if (WURFL.form_factor != 'Desktop' || window.innerWidth < 900) { // assume it's a mobile
 			mobile = true;
 			fps = 30;
+		}
+
+		if (WURFL.form_factor == 'Desktop') {
+			mobile = false;
+			fps = 60;
 		}
 
 		audioclip = $('#audioclip')[0];
