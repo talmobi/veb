031bcff0e7de9982f0f9cb5d0e27ce0742b61dd0
Hannyajin <talmo.christian@gmail.com>
2014-05-17 21:00:14 +0300

    added bower tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ var gulp = require('gulp'),
     minifycss = require('gulp-minify-css'),
     less = require('gulp-less'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify');
-
+    uglify = require('gulp-uglify'),
+    bower = require('gulp-bower-files');
 
 gulp.task('process-scripts', function() {
   return gulp.src('app/scripts/*.js')
@@ -18,12 +18,41 @@ gulp.task('process-scripts', function() {
 gulp.task('process-css', function() {
   return gulp.src('app/styles/*.css')
     .pipe(concat('main.css'))
-    .pipe(gulp.dest('dist/scripts'))
+    .pipe(gulp.dest('dist/styles'))
     .pipe(rename({suffix: '.min'} ))
     .pipe(minifycss())
-    .pipe(gulp.dest('dist/scripts'))
+    .pipe(gulp.dest('dist/styles'))
 });
 
+gulp.task('process-images', function() {
+  return gulp.src('app/images/*')
+    .pipe(gulp.dest('dist/images'))
+
+});
+
+gulp.task('process-music', function() {
+  return gulp.src('app/music/*')
+    .pipe(gulp.dest('dist/music'))
+
+});
+
+gulp.task('process-bower', function() {
+  return bower()
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'))
+})
+
+var verilyFiles = [
+  'app/.htaccess', 'app/404.html', 'app/favicon.ico', 'app/index.html', 'app/robots.txt'
+];
+// process client specific files
+gulp.task('process-verily-files', function() {
+  return gulp.src(verilyFiles)
+    .pipe(gulp.dest('dist'))
+});
 
 gulp.task('watch', function() {
   gulp.watch('src/scripts/*.js', ['process-scripts'])
